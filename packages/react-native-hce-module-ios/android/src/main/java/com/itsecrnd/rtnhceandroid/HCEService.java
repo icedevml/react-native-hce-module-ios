433586324ee5c7e110fa4faa3000a37e725d2e0f
Michał Leszczyński <ml@icedev.pl>
2025-09-13 07:42:30 +0200

    implement stub code

--- packages/react-native-hce-module-ios/android/src/main/java/com/itsecrnd/rtnhceandroid/HCEService.java
+++ packages/react-native-hce-module-ios/android/src/main/java/com/itsecrnd/rtnhceandroid/HCEService.java
@@ -1,26 +1,53 @@
 package com.itsecrnd.rtnhceandroid;
 
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.nfc.cardemulation.HostApduService;
 import android.os.Bundle;
 import android.util.Log;
 
+@SuppressLint("NewApi") // TODO remove
 public class HCEService extends HostApduService {
     private static final String TAG = "CardService";
 
-    public static final byte[] R_APDU_OK = BinaryUtils.HexStringToByteArray("0A9000");
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i(TAG, "Received broadcast in HCEService.");
+            String action = intent.getAction();
+
+            if (action != null && action.equals("com.itsecrnd.rtnhceandroid.sendresponseapdu")) {
+                sendResponseApdu(new byte[] { (byte) 0xCC, (byte) 0x90, (byte) 0x00 });
+            }
+        }
+    };
 
     @Override
     public byte[] processCommandApdu(byte[] command, Bundle extras) {
-        return R_APDU_OK;
+        Intent intent = new Intent("com.itsecrnd.rtnhceandroid.sendcommandapdu");
+        intent.putExtra("capdu", BinaryUtils.ByteArrayToHexString(command));
+        getApplicationContext().sendBroadcast(intent);
+
+        return null;
     }
 
     @Override
     public void onCreate() {
         Log.d(TAG, "Starting service");
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.itsecrnd.rtnhceandroid.sendresponseapdu");
+        registerReceiver(receiver, filter, RECEIVER_EXPORTED);
+        // TODO check security around RECEIVER_EXPORTED - we only need to communicate inside app
     }
 
     @Override
     public void onDeactivated(int reason) {
         Log.d(TAG, "Finishing service: " + reason);
+
+        unregisterReceiver(receiver);
     }
 }
--- packages/react-native-hce-module-ios/android/src/main/java/com/itsecrnd/rtnhceandroid/RTNHCEAndroidModule.java
+++ packages/react-native-hce-module-ios/android/src/main/java/com/itsecrnd/rtnhceandroid/RTNHCEAndroidModule.java
@@ -1,15 +1,44 @@
 package com.itsecrnd.rtnhceandroid;
 
+import static android.content.Context.RECEIVER_EXPORTED;
+
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 
 public class RTNHCEAndroidModule extends NativeHCEModuleSpec {
+    public static final String TAG = "RTNHCEAndroidModule";
     public static final String NAME = "NativeHCEModule";
 
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i(TAG, "Received broadcast in RTNHCEAndroidModule.");
+            String action = intent.getAction();
+
+            if (action != null && action.equals("com.itsecrnd.rtnhceandroid.sendcommandapdu")) {
+                Log.i(TAG, "Received C-APDU: " + intent.getStringExtra("capdu"));
+            }
+        }
+    };
+
+    @SuppressLint("NewApi")  // TODO remove
     RTNHCEAndroidModule(ReactApplicationContext context) {
         super(context);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction("com.itsecrnd.rtnhceandroid.sendcommandapdu");
+        getReactApplicationContext().getApplicationContext().registerReceiver(receiver, filter, RECEIVER_EXPORTED);
+
+        // TODO unregister receiver
     }
 
     @Override
@@ -25,7 +54,10 @@ public class RTNHCEAndroidModule extends NativeHCEModuleSpec {
 
     @Override
     public void acquireExclusiveNFC(Promise promise) {
-
+        Log.i(TAG, "Send broadcast.");
+        Intent intent = new Intent("com.itsecrnd.rtnhceandroid.sendresponseapdu");
+        intent.putExtra("rapdu", "EE9000");
+        this.getReactApplicationContext().getApplicationContext().sendBroadcast(intent);
     }
 
     @Override

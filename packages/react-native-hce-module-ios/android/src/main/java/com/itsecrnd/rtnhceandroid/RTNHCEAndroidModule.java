acf805cda8389ea471d2d719a412cb6f9d76bfdc
Michał Leszczyński <ml@icedev.pl>
2025-09-13 06:55:29 +0200

    update

--- packages/react-native-hce-module-ios/android/src/main/java/com/itsecrnd/rtnhceandroid/RTNHCEAndroidModule.java
+++ packages/react-native-hce-module-ios/android/src/main/java/com/itsecrnd/rtnhceandroid/RTNHCEAndroidModule.java
@@ -1,15 +1,9 @@
 package com.itsecrnd.rtnhceandroid;
 
 import androidx.annotation.NonNull;
-import com.facebook.react.bridge.NativeModule;
+
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import java.util.Map;
-import java.util.HashMap;
-import com.itsecrnd.rtnhceandroid.NativeHCEModuleSpec;
 
 public class RTNHCEAndroidModule extends NativeHCEModuleSpec {
     public static final String NAME = "NativeHCEModule";
--- packages/react-native-hce-module-ios/android/src/main/java/com/itsecrnd/rtnhceandroid/RTNHCEAndroidPackage.java
+++ packages/react-native-hce-module-ios/android/src/main/java/com/itsecrnd/rtnhceandroid/RTNHCEAndroidPackage.java
@@ -1,16 +1,13 @@
 package com.itsecrnd.rtnhceandroid;
 
 import androidx.annotation.Nullable;
+
+import com.facebook.react.TurboReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.module.model.ReactModuleInfo;
 import com.facebook.react.module.model.ReactModuleInfoProvider;
-import com.facebook.react.TurboReactPackage;
-import com.facebook.react.uimanager.ViewManager;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
 

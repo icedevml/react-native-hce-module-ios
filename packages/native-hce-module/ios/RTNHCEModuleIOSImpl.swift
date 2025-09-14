b2bda6d53707fd057f2720803361933e65f49e95
Michał Leszczyński <ml@icedev.pl>
2025-09-14 18:23:32 +0200

    iOS: Encode hex strings in upper case to match with Android behavior

--- packages/native-hce-module/ios/RTNHCEModuleIOSImpl.swift
+++ packages/native-hce-module/ios/RTNHCEModuleIOSImpl.swift
@@ -13,13 +13,8 @@ extension StringProtocol {
 }
 
 extension Data {
-  struct HexEncodingOptions: OptionSet {
-    let rawValue: Int
-    static let upperCase = HexEncodingOptions(rawValue: 1 << 0)
-  }
-
-  func hexEncodedString(options: HexEncodingOptions = []) -> String {
-    let format = options.contains(.upperCase) ? "%02hhX" : "%02hhx"
+  func hexEncodedString() -> String {
+    let format = "%02hhX"
     return map { String(format: format, $0) }.joined()
   }
 }

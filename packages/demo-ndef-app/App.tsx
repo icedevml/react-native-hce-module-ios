763e6d03612f05acfe54e74848de3fc174eade15
Michał Leszczyński <ml@icedev.pl>
2025-09-21 22:34:17 +0200

    Demo app: enhance demos to cover for more use cases (#60)

--- packages/demo-ndef-app/App.tsx
+++ packages/demo-ndef-app/App.tsx
@@ -1,103 +1,41 @@
 import React from 'react';
-import { Alert, Button, StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-import { Buffer } from 'buffer/';
-
-import NativeHCEModule, {HCEModuleEvent} from '@icedevml/react-native-host-card-emulation/js/NativeHCEModule';
-import { createNDEFType4TagApp } from './ndefType4TagApp.ts';
+import MainView from './components/MainView.tsx';
+import UserInteractionHCEView from './components/UserInteractionHCEView.tsx';
+import PresentmentHCEView from './components/PresentmentHCEView.tsx';
+import ContinousHCEView from './components/ContinousHCEView.tsx';
 
 function App(): React.JSX.Element {
-  const [recentEventsList, setRecentEventsList] = React.useState<string[]>([]);
-  const ndefApp = React.useMemo(() => createNDEFType4TagApp(), []);
+  const [currentView, setCurrentView] = React.useState<string>("main");
 
-  React.useEffect(() => {
-    NativeHCEModule?.onEvent(async (event: HCEModuleEvent) => {
-      try {
-        console.log('received event', event);
-        setRecentEventsList((prevState) => {
-          const data = [...prevState, event.type + " " + event.arg];
-          return data.slice(-15);
-        });
-
-        switch (event.type) {
-          case 'readerDetected':
-            NativeHCEModule?.setSessionAlertMessage('Reader detected');
-            break;
-
-          case 'readerDeselected':
-            NativeHCEModule?.setSessionAlertMessage('Lost reader');
-            break;
-
-          case 'sessionStarted':
-            NativeHCEModule?.setSessionAlertMessage(
-              'Tap towards the reader',
-            );
-            await NativeHCEModule?.startHCE();
-            break;
-
-          case 'sessionInvalidated':
-            if (event.arg === 'maxSessionDurationReached') {
-              Alert.alert(
-                'Information',
-                'Session has timed out and thus was invalidated ' +
-                  'by the operating system.',
-              );
-            }
-            break;
-
-          case 'received':
-            const capdu = Buffer.from(event.arg!, 'hex');
-            await NativeHCEModule?.respondAPDU(null, ndefApp(capdu).toString("hex"));
-            break;
-        }
-      } catch (err) {
-        console.error('error in event handler', err);
-      }
-    });
-  }, [ndefApp]);
-
-  async function doBeginSession() {
-    try {
-      await NativeHCEModule?.beginSession();
-    } catch (error) {
-      console.error('beginSession err', error);
-      Alert.alert('Error', 'Failed to begin NFC session: ' + error);
-      return;
+  const renderCurrentView = () => {
+    if (currentView === 'main') {
+      return <MainView setCurrentView={setCurrentView} />;
+    } else if (currentView === 'user-interaction') {
+      return <UserInteractionHCEView setCurrentView={setCurrentView} />;
+    } else if (currentView === 'ios-presentment') {
+      return <PresentmentHCEView setCurrentView={setCurrentView} />;
+    } else if (currentView === 'continous') {
+      return <ContinousHCEView setCurrentView={setCurrentView} />;
     }
-  }
-
-  async function doAcquireExclusiveNFC() {
-    try {
-      await NativeHCEModule?.acquireExclusiveNFC();
-    } catch (error) {
-      console.error('acquireExclusiveNFC err', error);
-      Alert.alert('Error', 'Failed to acquire exclusive NFC access: ' + error);
-      return;
-    }
-
-    Alert.alert(
-      'Information',
-      'Acquired exclusive NFC access for 15 seconds. ' +
-        "Operating system's services (like background tag reading) are disabled within that period.",
-    );
-  }
-
-  let recentEvents = recentEventsList.join('\n');
+  };
 
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.safeAreaView}>
-        <Text style={styles.text}>Demo App for Native HCE Module</Text>
-        <Text style={styles.text}>
+        <Text style={styles.headerText}>Demo App for Native HCE Module</Text>
+        <Text style={styles.headerText}>
           GitHub: icedevml/react-native-host-card-emulation
         </Text>
-        <Button title="Begin HCE session&emulation" onPress={doBeginSession} />
-        <Button
-          title="Acquire exclusive NFC access"
-          onPress={doAcquireExclusiveNFC}
+        <View
+          style={{
+            borderBottomColor: 'black',
+            borderBottomWidth: StyleSheet.hairlineWidth,
+          }}
         />
-        <Text style={styles.text}>{recentEvents}</Text>
+        {renderCurrentView()}
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -111,6 +49,12 @@ const styles = StyleSheet.create({
   text: {
     margin: 10,
     fontSize: 20,
+    color: 'black',
+  },
+  headerText: {
+    margin: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
     color: 'black',
   },
   textInput: {
--- packages/demo-ndef-app/background.ts
+++ packages/demo-ndef-app/background.ts
@@ -7,13 +7,13 @@ import { createNDEFType4TagApp } from './ndefType4TagApp.ts';
 
 
 export default async function runBackgroundHCETask(processBackgroundHCE: ProcessBackgroundHCEFunc) {
-  const ndefApp = createNDEFType4TagApp()
+  const [_getNDEFAppState, _resetNDEFAppState, handleCAPDU] = createNDEFType4TagApp()
 
   processBackgroundHCE(async (event, respondAPDU) => {
     switch (event.type) {
       case 'received':
         const capdu = Buffer.from(event.arg!, 'hex');
-        await respondAPDU(ndefApp(capdu).toString("hex"));
+        await respondAPDU(handleCAPDU(capdu).toString("hex"));
         break;
 
       case 'readerDeselected':
